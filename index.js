ed0aa7e99bbf0759827ef296f1829ae9b2eb2609
Frantz Crozilhac <fran.stacksta@gmail.com>
2020-12-30 23:38:29 +0100

    Start Register & Login Users - add nodemon + cleaning DX

--- index.js
+++ index.js
@@ -3,34 +3,9 @@ const gql = require('graphql-tag')
 const mongoose = require('mongoose');
 
 const { MONGODB } = require('./config.js')
-const Post = require('./models/Post')
-const User = require('./models/User')
+const resolvers = require('./graphql/resolvers/index')
+const typeDefs = require('./graphql/typeDefs')
 
-const typeDefs = gql`
-  type Post{
-    id: ID!
-    body: String!
-    createdAt: String!
-    username:String!
-  }
-  type Query{
-    getPosts: [Post]
-  }
-`
-
-const resolvers = {
-  Query:{
-    async getPosts() {
-      try{
-        const posts = await Post.find();
-        return posts;
-      }
-      catch(err){
-        throw new Error(err);
-      }
-    }
-  }
-}
 
 
 const server = new ApolloServer({
