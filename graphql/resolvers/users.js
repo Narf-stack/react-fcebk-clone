b229d3db48a2fb31c1cdd64e0649cd40299589c6
Frantz Crozilhac <fran.stacksta@gmail.com>
2020-12-31 16:38:33 +0100

    End Register & Login Users - Add Login Mutation

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -2,9 +2,21 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const {UserInputError}= require('apollo-server')
 
-const {validateRegisterInput,validateLoginInput} = require('../../util/validators')
+const {validateRegisterInput, validateLoginInput} = require('../../util/validators')
 const User = require('../../models/User')
 const {SECRET_KEY} = require('../../config')
+
+function generateToken(user){
+  return jwt.sign(
+        {
+          id: user.id,
+          email: user.email,
+          username: user.username
+        },
+        SECRET_KEY,
+        {expiresIn:'1h'}
+      );
+}
 
 module.exports = {
   Query:{
@@ -20,6 +32,33 @@ module.exports = {
     }
   },
   Mutation:{
+    async login(_,{username,password}){
+      const {errors, valid } = validateLoginInput(username,password)
+      const user = await User.findOne({username})
+
+      if(!valid){
+        throw new UserInputError('Errors',{errors})
+      }
+      if(!user){
+        errors.general = 'User not found';
+        throw new UserInputError('User not found',{errors})
+      }
+
+      const match = await bcrypt.compare(password,user.password)
+      if(!match){
+        errors.general = 'Wrong credentials';
+        throw new UserInputError('Wrong credentials',{errors})
+      }
+
+      const token = generateToken(user)
+
+      return{
+        ...user._doc,
+        id: user._id,
+        token
+      };
+    },
+
     async register(_,
       {
         registerInput:{username,email,password,confirmPassword}
@@ -50,11 +89,7 @@ module.exports = {
 
       const res = await newUser.save();
 
-      const token = jwt.sign({
-        id: res.id,
-        email: res.email,
-        username: res.username
-      },SECRET_KEY,{expiresIn:'1h'});
+      const token = generateToken(res)
 
       // console.log(res)
       return{
